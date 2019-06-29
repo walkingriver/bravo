5a9d1792cb764371656b726e5b969c954e47c1e6
Michael Callaghan <mikecallaghan67@gmail.com>
2019-06-29 14:44:55 -0400

    Code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Platform, AlertController, NavController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { GameStorageService } from './game-storage.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -31,7 +29,6 @@ export class AppComponent implements OnInit {
 
   constructor(
     private platform: Platform,
-    private router: Router,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
 
