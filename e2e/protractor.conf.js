dd0838dd8442371bfa2c00874c262cd94fa15307
Michael Callaghan <mikecallaghan67@gmail.com>
2020-08-12 18:12:46 -0400

    Added two protractor devices

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -33,6 +33,20 @@ exports.config = {
     }, {
       browserName: 'chrome',
       chromeOptions: {
+        mobileEmulation: {
+          'deviceName': 'Nexus 7'
+        }
+      }
+    }, {
+      browserName: 'chrome',
+      chromeOptions: {
+        mobileEmulation: {
+          'deviceName': 'Nexus 10'
+        }
+      }
+    }, {
+      browserName: 'chrome',
+      chromeOptions: {
         args: ['--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"'],
 
         mobileEmulation: {
